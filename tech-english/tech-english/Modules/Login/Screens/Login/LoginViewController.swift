f5c8d753c925959a5f10c86e923c993f1017a0a2
William Reinert <williamreinert@hotmail.com>
2022-09-12 22:32:54 -0300

    Adicionar funcionalidade do botão de esconder senha

--- tech-english/tech-english/Modules/Login/Screens/Login/LoginViewController.swift
+++ tech-english/tech-english/Modules/Login/Screens/Login/LoginViewController.swift
@@ -18,22 +18,30 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var mainStackView: UIStackView!
     @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     private func setupUI() {
         signInButton.layer.cornerRadius = 6
         contentView.layer.cornerRadius = 15
+        
+        let emailImage = "mail-icon"
+        let passwordImage = "lock-icon"
+        let revealPasswordImage = "password-icon"
+        emailTextField.setupImage(imageName: emailImage, on: .left, x: 18, y: 10, width: 20, height: 20)
+        passwordTextField.setupImage(imageName: passwordImage, on: .left, x: 18, y: 10, width: 20, height: 20)
+        passwordTextField.setupImage(imageName: revealPasswordImage, on: .right, x: 0, y: 10, width: 20, height: 20)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTogglePasswordButtonTapped(sender:)))
+        passwordTextField.rightView?.addGestureRecognizer(tapGesture)
     }
-}
+    
+    @objc func handleTogglePasswordButtonTapped(sender: UIGestureRecognizer) {
+        if passwordTextField.isSecureTextEntry == true {
+            passwordTextField.isSecureTextEntry = false
+        } else {
+            passwordTextField.isSecureTextEntry = true
+        }
+    }
 
-//extension UIView {
-//    func customize(backgroundColor: UIColor = .clear, radiusSize: CGFloat = 0) {
-//        let subView = UIView(frame: bounds)
-//        subView.backgroundColor = backgroundColor
-//        subView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        insertSubview(subView, at: 0)
-//
-//        subView.layer.cornerRadius = radiusSize
-//        subView.layer.masksToBounds = true
-//        subView.clipsToBounds = true
-//    }
-//}
+}
---
+++ tech-english/tech-english/Utils/Extensions/ UITextField+extensions.swift
@@ -0,0 +1,37 @@
+//
+//   UITextField + extensions.swift
+//  tech-english
+//
+//  Created by William Reinert on 12/09/22.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    enum TextFieldImageSide {
+        case left
+        case right
+    }
+
+    func setupImage(imageName: String, on side: TextFieldImageSide, x: Int, y: Int, width: Int, height: Int) {
+        let imageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: height))
+        if let imageWithSystemName = UIImage(systemName: imageName) {
+            imageView.image = imageWithSystemName
+        } else {
+            imageView.image = UIImage(named: imageName)
+        }
+        
+        let imageContainerView = UIView(frame: CGRect(x: 0, y: 0, width: width * 2, height: height * 2))
+        imageContainerView.addSubview(imageView)
+        
+        switch side {
+        case .left:
+            leftView = imageContainerView
+            leftViewMode = .always
+        case .right:
+            rightView = imageContainerView
+            rightViewMode = .always
+        }
+    }
+}
